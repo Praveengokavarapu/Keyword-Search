fc0574d17f5d9129666c631b2e37dd94a4141b19
Praveengokavarapu <96908000+Praveengokavarapu@users.noreply.github.com>
2023-08-22 21:51:05 +0530

    Update client.js

--- client.js
+++ client.js
@@ -5,12 +5,12 @@ const getresults=(event)=>{
     event.preventDefault()
     const keyword=document.getElementsByClassName("search")[0].value
     const API_KEY="AIzaSyCYnZgqkHzOs_AUarno_uW4g0nzsCR8o4Q"
-    fetch(`https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(keyword)}&key=${API_KEY}`)
+    fetch(`https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(keyword)}&key=${API_KEY}publishedAfter=2022-08-23T00%3A00%3A00Z&publishedBefore=2023-08-23T00%3A00%3A00Z`)
     .then(response=>response.json())
     .then(data=>{
         
         console.log(data)
-        let results=data.pageInfo.totalResults;
+        let results=parseInt(data.pageInfo.totalResults/12);
         if (results===1000000){
             document.getElementById("search-output").innerHTML=`Woah !! "${keyword}" has appeared <span class="span">${results}+</span> times in YouTube`
 
